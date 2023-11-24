44dc96b73477f64fb6b14fdfe72c5e2e6ada8d6f
ASUS <ASUS@LAPTOP-1O8L77VP>
2023-11-24 16:30:46 +0530

    Adding new test for patient identifier types

---
+++ selenium_2023/src/test/java/org/h2k/openmrs/tests/Manage_Patient_Identfier_Types_Tests.java
@@ -0,0 +1,12 @@
+package org.h2k.openmrs.tests;
+
+import org.testng.annotations.Test;
+
+public class Manage_Patient_Identfier_Types_Tests {
+
+	@Test
+	public void validate_add_patient_identifier_types()
+	{
+		
+	}
+}
